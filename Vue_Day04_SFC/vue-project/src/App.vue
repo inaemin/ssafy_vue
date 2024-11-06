5fcc83a2d960c5ab155d469faeff9e6a0f48f0f2
= <godqhr5627@gmail.com>
2024-11-06 10:54:05 +0900

    upload

---
+++ Vue_Day04_SFC/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>App.vue</h2>
+    <MyComponent/>
+    <my-component></my-component>
+  </div>
+</template>
+
+<script setup>
+  //import MyComponent from './components/MyComponent.vue'; //상대경로
+  import MyComponent from '@/components/MyComponent.vue';   //절대경로 @ == src
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue_Day04_SFC/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>MyComponent</h3>
+    <MyComponentItem/>
+    <MyComponentItem/>
+    <MyComponentItem/>
+    <MyComponentItem/>
+  </div>
+</template>
+
+<script setup>
+  // import MyComponentItem from './MyComponentItem.vue';
+  import MyComponentItem from '@/components/MyComponentItem.vue';
+
+</script>
+
+<style scoped>
+
+</style>
