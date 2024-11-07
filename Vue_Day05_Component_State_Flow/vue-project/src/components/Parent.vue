790535d6b125f820004a365f5a7725a705037c82
YangMG <godqhr5627@gmail.com>
2024-11-07 10:51:29 +0900

    upload

---
+++ Vue_Day05_Component_State_Flow/vue-project/src/components/Parent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>부모 컴포넌트</h3>
+        <ParentChild @update-nameeee="updateNameeeee"  @some-event="someCallback" @emit-argss="getNumber"
+        my-msg="message" :dynamic-name="name" :person="person"/>
+    </div>
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+    import ParentChild from './ParentChild.vue';
+
+    const name = ref("Kim")
+    const person = ref({
+        name:"Yang",
+        age: 45
+    })
+
+    const someCallback = function() {
+        console.log("어떠한 이벤트가 발생했습니다.")
+    }
+
+    const getNumber = function(...args){
+        console.log(args)
+    }
+
+    const updateNameeeee = function(newName){
+        name.value = newName
+    }
+
+</script>
+
+<style  scoped>
+
+</style>
---
+++ Vue_Day05_Component_State_Flow/vue-project/src/components/ParentGrandChild.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h5>손자컴포넌트</h5>
+        <p>{{ myMsg }}</p>
+        <input type="text" v-model="name">
+        <p>신청 이름 : {{ name }}</p>
+        <button @click="updateName">이름 개명 신청</button>
+    </div>
+</template>
+
+<script setup>
+    import {ref} from 'vue'
+    defineProps({
+        myMsg: String
+    })
+
+    const emit = defineEmits(['updateNamee'])
+    const updateName = function(){
+        emit('updateNamee', name.value)
+        name.value = "" //개명 신청 후 input 창 비웠다.
+    }
+
+    const name = ref("")
+
+</script>
+
+<style scoped>
+
+</style>
