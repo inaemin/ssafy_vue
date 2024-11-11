e5af8f992d8c6a134f309e2b3ad451a35b69129c
YangMG <godqhr5627@gmail.com>
2024-11-11 12:00:03 +0900

    upload

---
+++ Vue_Day07_StateManagement/vue-project/src/components/StoreTest.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- 기록용 -->
+    <div>  
+      <h2>StoreTest</h2>
+      <!-- State 저장되어 있는 count 보고싶어 -->
+      <p>state : {{ store.count }}</p>
+      <!-- Getters 쓰고 싶어 -->
+      <p>getter : {{ store.doubleCount }}</p>
+      <button @click="store.increment()">증가1</button>
+      <button @click="increment2">증가2</button>
+    </div>
+  </template>
+  
+  <script setup>
+    import { useCounterStore } from '@/stores/counter';
+  
+    const store = useCounterStore()
+  
+    console.log(store.count)
+  
+    const increment2 = function(){
+      store.increment()
+    }
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
---
+++ Vue_Day07_StateManagement/vue-project/src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h4>TodoList</h4>
+        <TodoListItem v-for="todoo in store.todos" :key="todoo.id" :todo="todoo"/>
+    </div>
+</template>
+
+<script setup>
+    import TodoListItem from './TodoListItem.vue';
+    import { useTodosStore } from '@/stores/todos';
+
+    const store = useTodosStore();
+</script>
+
+<style scoped>
+
+</style>
